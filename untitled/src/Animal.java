23fa0aaf2b0cd8cf9a77acbd0a14c6adc14554a2
bunnyyrr <kate_kondrashova07@mail.ru>
2026-08-05 15:41:33 +0700

    добавила конструктор и исправила опечатки

--- untitled/src/Animal.java
+++ untitled/src/Animal.java
@@ -2,6 +2,11 @@ abstract class Animal {
     private String name;
     private int age;
 
+    public Animal(String name, int age){
+        this.name = name;
+        this.age = age;
+    }
+
     public String getName() {
         return name;
     }
@@ -9,12 +14,12 @@ abstract class Animal {
         return age;
     }
 
-    public void setName(String newName) {
-        if(newName== null || newName.isEmpty()) throw new IllegalArgumentException("Имя не может быть null или пустым");
-        else name = newName;
+    public void setName(String name) {
+        if(name== null || name.isEmpty()) throw new IllegalArgumentException("Имя не может быть null или пустым");
+        else this.name = name;
     }
-    public void setAge(int newAge) {
-        if(newAge>0) age = newAge;
+    public void setAge(int age) {
+        if(age>0) this.age = age;
         else throw new IllegalArgumentException("Возраст не может быть отрицательным");
     }
 
