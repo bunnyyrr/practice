6301893ff53922b4de9bbba354f133e45b50e039
bunnyyrr <kate_kondrashova07@mail.ru>
2026-08-10 15:20:13 +0700

    практиковалась с коллекциями, простой хэш на HashMap

---
+++ untitled/src/PracticeException/SquareCache.java
@@ -0,0 +1,17 @@
+package PracticeException;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class SquareCache {
+    private Map<Integer, Integer> cache = new HashMap<>();
+
+    public int getSquare(int num){
+        if(cache.containsKey(num)) return cache.get(num);
+        else {
+            int square = num*num;
+            cache.put(num, square);
+            return cache.get(square);
+        }
+    }
+}
