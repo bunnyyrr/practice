aa37ab0bd663484416ca0b6715a0de7ecae500dd
bunnyyrr <kate_kondrashova07@mail.ru>
2026-08-11 14:27:07 +0700

    практиковалась с PostgereSQL

---
+++ untitled/src/SQLPractice/Orders.sql
@@ -0,0 +1,52 @@
+CREATE TABLE customers(
+                          id SERIAL PRIMARY KEY,
+                          full_name VARCHAR(100),
+                          email VARCHAR(100) UNIQUE NOT NULL,
+                          is_active BOOLEAN DEFAULT TRUE
+);
+
+CREATE TABLE orders(
+                       id SERIAL PRIMARY KEY,
+                       customer_id INTEGER REFERENCES customers(id) ON DELETE CASCADE,
+                       amount NUMERIC(10,2) CHECK(amount>=0),
+                       created_at TIMESTAMPTZ DEFAULT now()
+);
+
+CREATE TABLE products(
+                         id SERIAL PRIMARY KEY,
+                         name VARCHAR(100),
+                         price NUMERIC(10, 2)
+);
+
+ALTER TABLE orders ADD COLUMN product_id INTEGER REFERENCES products(id);
+
+INSERT INTO customers (full_name, email) VALUES
+                                             ('Анна', 'anna@mail.com'),
+                                             ('Мария', 'maria@mail.com'),
+                                             ('Вика', 'oleg@mail.ru');
+iNSERT INTO products (name, price) VALUES
+                                       ('Кружка', 350),
+                                       ('Блокнот', 200),
+                                       ('Наушники', 2500);
+
+SELECT id, full_name FROM customers;
+SELECT id, name FROM products;
+INSERT INTO orders(customer_id, product_id) VALUES
+                                                ('1', '1'),
+                                                ('1', '2'),
+                                                ('2', '1');
+
+SELECT * FROM orders;
+
+SELECT c.full_name, o.id, p.name
+FROM orders o
+         JOIN customers c ON o.customer_id=c.id
+         JOIN products p ON o.product_id=p.id;
+
+SELECT c.full_name AS name, o.id
+FROM customers c
+         LEFT JOIN orders o ON c.id=o.customer_id;
+
+SELECT p.name, o.id AS order
+FROM products p
+    FULL JOIN orders o ON p.id = o.product_id;
