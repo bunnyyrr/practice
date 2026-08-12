842db5de0445bda45c5daefb38058190477611b9
bunnyyrr <kate_kondrashova07@mail.ru>
2026-08-12 16:18:17 +0700

    практиковалась с PostgereSQL

--- untitled/src/SQLPractice/Orders.sql
+++ untitled/src/SQLPractice/Orders.sql
@@ -67,7 +67,7 @@ SELECT full_name, SUM(amount)
 FROM customers c
          LEFT JOIN orders o ON customer_id = c.id
 GROUP BY c.id
-HAVING SUM(amount)>300;;
+HAVING SUM(amount)>300;
 
 ALTER TABLE customers ADD COLUMN phone VARCHAR(15) UNIQUE;
 
@@ -81,3 +81,65 @@ WHERE id =3;
 
 SELECT full_name, COALESCE(phone, 'не указан')
 FROM customers;
+
+INSERT INTO products(name, price) VALUES
+    ('Шоколадка', 100) RETURNING id;
+
+SELECT p.name
+FROM products p
+WHERE NOT EXISTS(
+    SELECT 1
+    FROM orders o
+    WHERE o.product_id = p.id
+);
+
+WITH totals AS(
+    SELECT customer_id, SUM(o.amount) AS total_spent
+    FROM orders o
+    GROUP BY customer_id
+)
+SELECT c.full_name , t.total_spent
+FROM totals t
+         JOIN customers c ON c.id=t.customer_id
+WHERE t.total_spent > (SELECT AVG(amount)
+                       FROM orders);
+
+
+EXPLAIN SELECT * FROM orders WHERE customer_id=1;
+
+CREATE INDEX idx_orders_customer_id ON orders (customer_id);
+
+SELECT COUNT(*) FROM orders;
+SELECT COUNT(*) FROM orders WHERE customer_id = 1;
+
+EXPLAIN ANALYZE SELECT * FROM orders WHERE customer_id = 1;
+
+SELECT phone FROM customers WHERE id=2;
+
+BEGIN;
+UPDATE customers SET phone='+79007654322' WHERE id=2;
+SELECT phone FROM customers WHERE id=2;
+ROLLBACK;
+
+SELECT phone FROM customers WHERE id=2;
+
+
+ALTER TABLE products ADD COLUMN attributes JSONB;
+
+UPDATE products SET attributes='{"цвет": "желтый", "высота": "10см"}'
+WHERE id=1;
+UPDATE products SET attributes='{"цвет": "коричневый", "материал": "картон"}'
+WHERE id=2;
+UPDATE products SET attributes='{"цвет": "белый"}'
+WHERE id=3;
+
+SELECT name FROM products WHERE attributes@>'{"материал": "картон"}';
+SELECT attributes->>'цвет' FROM products WHERE name='Наушники';
+
+
+INSERT INTO customers(full_name, email)
+VALUES ('Анютка', 'anna@mail.com')
+    ON CONFLICT (email) DO UPDATE SET fuLl_name= EXCLUDED.full_name;
+
+SELECT full_name from customers WHERE email='anna@mail.com';
+
